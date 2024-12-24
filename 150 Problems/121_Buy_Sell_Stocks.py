a1468c2a958beea3d84581027c45f1ce907514b5
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-12-25 01:24:19 +0500

    solved

--- 150 Problems/121_Buy_Sell_Stocks.py
+++ 150 Problems/121_Buy_Sell_Stocks.py
@@ -4,16 +4,15 @@ class Solution:
     def maxProfit(self, prices) -> int:
         if len(prices) == 1: return 0
         max_profit = 0
-        for i, val in enumerate(prices):
-            profit = 0
-            for j in range(i+1, len(prices)):
-                res = prices[j] - prices[i]
-                if res > profit:
-                    profit = res
-            if profit > max_profit:
-                max_profit = profit
+        buy_price = prices[0]
+        for i in range(1, len(prices)):
+            max_profit = max(max_profit, prices[i] - buy_price)
+
+            if prices[i] < buy_price:
+                buy_price = prices[i]
         return max_profit
 
 sol = Solution()
 prices = [7,1,5,3,6,4]
+prices = [7,6,4,3,1]
 print(sol.maxProfit(prices))
